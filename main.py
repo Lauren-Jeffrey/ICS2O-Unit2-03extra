b488628648f3a155e0e09aa09a3efe2600bd3a5b
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2020-11-26 15:21:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Lauren Jeffrey
+
+# Created on: November 2020
+
+# This is the "Hello, World!" program
+
+def main():
+
+    # this function prints to console
+
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+
+    main()
